5c9ad05124def6779ecfcf8bdcbee863106fe291
SrWillxD <willgra09@hotmail.com>
2023-12-06 21:44:23 -0300

    Setting EJS and the Views folder as the holder of the files to be delivered.

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const routes = require('./app/Routes/router.js')
+const routes = require('./app/Routes/router.js');
 
 const port = 3333;
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname + '/app/Public'));
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/app/Public/Views');
 
 app.use('/', routes);
 
-
-app.listen(3333, () => console.log(`⚡🚪 Backend started at http://localhost:${port}`));
+app.listen(port, () => console.log(`⚡🚪 Backend started at http://localhost:${port}`));
