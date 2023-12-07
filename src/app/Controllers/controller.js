ec55d3dead45ee68e2749e0711e18c5af1ba6b6b
SrWillxD <willgra09@hotmail.com>
2023-12-06 21:37:04 -0300

    Changing the page delivery method to use res.render.

--- src/app/Controllers/controller.js
+++ src/app/Controllers/controller.js
@@ -1,20 +1,8 @@
-const fs = require('fs');
-const path = require('path');
-
 const controllerOBJ = {
     home(req, res, next){
-        try{
-            const filePath = path.join(__dirname, '../Public/HTML/home.html');
-
-            fs.readFile(filePath, 'utf8', (err, data)=>{
-                if(err){throw err;}
-                res.send(data);
-            });
-        }catch(err){
-            console.error("Error displaying home", err);
-            res.status(500).json({ message: 'Internal Server Error.'});
-        }
+        res.render('home');
     }
 }
+
 
 module.exports = controllerOBJ;
