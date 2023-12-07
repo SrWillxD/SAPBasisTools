a9ae2fee0ffbb962c8228ff28b822d2f37395f4c
SrWillxD <willgra09@hotmail.com>
2023-12-06 22:02:58 -0300

    Writing the first test with Cypress.

--- cypress/e2e/tableBehavior/table.cy.js
+++ cypress/e2e/tableBehavior/table.cy.js
@@ -1,1 +1,11 @@
 /// <reference types="Cypress"/>
+
+describe('Check if the link is active', () => {
+    it('Status 2xx', () => {
+        const linkUrl = 'http://localhost:3333';
+
+        cy.request(linkUrl).as('linkRequest');
+
+        cy.get('@linkRequest').its('status').should('be.oneOf', [200, 201, 202, 204]);
+    });
+});
