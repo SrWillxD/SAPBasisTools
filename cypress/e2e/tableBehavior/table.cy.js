197d9e9f918a270c00e2a0e3fc2ea8e889993fa8
SrWillxD <willgra09@hotmail.com>
2023-12-12 23:32:51 -0300

    Adding test to check if all fields in the model user's 'Funções' table were filled in.

--- cypress/e2e/tableBehavior/table.cy.js
+++ cypress/e2e/tableBehavior/table.cy.js
@@ -1,8 +1,7 @@
 /// <reference types="Cypress"/>
 
 describe('Paste replication behavior', ()=>{
-
-    const data = `/STMC/USER_ID	USR_TDZLGAJ6HAPORO
+    const parameterData = `/STMC/USER_ID	USR_TDZLGAJ6HAPORO
 AREA_ID	S_AREA_CMG
 BCS_ADMIN_TREE	CC
 CAC	A000
@@ -20,7 +19,26 @@ WLC	X   X  XX   X 0000
 RSWAD_SKIP_JAVA	X
 SCL	                                         G`
 
-    it('Must fill all fields', ()=>{
+    const roleData = `SAP_S_RFCACL
+ZNWBC_AUTO_TEST_CANVAS_ROLE_1
+ZNWBC_HIDE_TEST
+ZTRUSTEDSYSTEM
+ABAP_S4
+Z_ANALISTAS_SAP
+Z_BASIS_ARAUJO
+Z_CORP_SOLICITANTE_V_A_P
+Z_CORP_USUARIOS
+Z_DIGITAL_TIME_TECNOLOGIA
+PFCG_OBG
+Z_DROGATEL_GESTAO
+Z_GEINF_ANALISTAS_FUNCIONAIS
+Z_GETEC_AUXILIAR_SF
+SAP_SUP_FI
+Z_TEMP_SE38
+Z_USERS
+`
+
+    /*it('Must fill all fields', ()=>{
         cy.visit('http://localhost:3333/params');
 
         cy.get(':nth-child(1) > tbody > :nth-child(1) > :nth-child(1)')
@@ -81,7 +99,7 @@ SCL	                                         G`
 
         cy.get(':nth-child(1) > tbody > :nth-child(17) > :nth-child(1)').should('have.text', 'SCL');
         cy.get(':nth-child(1) > tbody > :nth-child(17) > :nth-child(2)').should('have.text', '                                         G');*/
-
+/*
         cy.get('.table').each((table, tableIndex) => {
             expectedData.forEach((rowData, rowIndex) => {
                 rowData.forEach((cellData, cellIndex) => {
@@ -92,5 +110,22 @@ SCL	                                         G`
                 });
             });
         });
+    });*/
+
+    it("Must fill in all the fields in the model user's 'Funções' table.", ()=>{
+        const roleDataArray = roleData.trim().split('\n');
+
+        cy.visit('http://localhost:3333/params');
+
+        cy.get('#modelUser > :nth-child(1) > tbody > :nth-child(1) > td')
+            .invoke('text', roleData)
+            .trigger('paste', { clipboardData: { getData: () => roleData } });
+
+        for(let i = 0; i < roleDataArray.length; i++){
+            const selector = `#modelUser > :nth-child(1) > tbody > :nth-child(${i + 1}) > td`;
+            cy.get(selector).should('have.text', roleDataArray[i]);
+            console.log(roleDataArray[i]);
+        }
     });
+
 });
