8fff0ba5f2efabd9176d0ef2e4bca469e3d90ced
SrWillxD <willgra09@hotmail.com>
2024-01-01 18:40:09 -0300

    Creating new tests to verify the result.

--- cypress/e2e/tableBehavior/findElements.cy.js
+++ cypress/e2e/tableBehavior/findElements.cy.js
@@ -1,24 +1,36 @@
 /// <reference types="Cypress"/>
 
 describe('Find elements', ()=>{
-
     it("Find button compareButtonBTN", ()=>{
         cy.visit('http://localhost:3333/params');
         cy.get('#compareButtonBTN');
     });
+});
 
-    it("When the 'Compare' button is clicked without any content in the tables, the result div should not be displayed.", ()=>{
-        cy.visit('http://localhost:3333/params');
-    });
+describe("When the 'Compare' button is clicked without any content in the tables, the result div should not be displayed.", ()=>{
+    
+});
 
-    it("When the 'Compare' button is clicked with real data in the table, but it is not necessary to perform any action, the corresponding result div should be shown informing that the users are already in sync.", ()=>{
-        cy.visit('http://localhost:3333/params');
-    });
+describe("When the 'Compare' button is clicked with real data in the table, but it is not necessary to perform any action, the corresponding result div should be shown informing that the users are already in sync.", ()=>{
 
-    it("When entering duplicate data, the result must be shown without duplicate values.", () => {
-        
-    });
+});
 
-    //! Create tests to check whether the element that shows the result is found when the button is clicked and the user requires actions.
-    //! Check the feasibility of creating describes for each item entered.
+describe("When entering duplicate data, the result must be shown without duplicate values.", ()=>{
+    
+});
+
+describe("When the value conversion button is clicked, the user model values must change table with the user copy.", ()=>{
+
+});
+
+describe("When adding more than 17 rows to the tables, they must expand according to the amount.", ()=>{
+
+});
+
+describe("Check the veracity of the results when changes are required for the user.", ()=>{
+
+});
+
+describe("When a result already exists, remove it before inserting the new one.", ()=>{
+
 });
